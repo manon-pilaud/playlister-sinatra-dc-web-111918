83ffc675db65fb85c002e59f47ea911f277a35e9
github username <github email address>
2018-12-13 12:35:40 -0500

    Checked boxes for songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,7 @@
 class SongsController < ApplicationController
+
 # set :views, './views/songs'
+
   get '/songs' do
     @songs = Song.all
     erb :'/songs/index'
@@ -9,6 +11,22 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
     erb :'/songs/show'
   end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  get '/songs/:id/edit' do
+    @song = Song.find(params[:id])
+    # @genres = Genre.all
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:id' do
+    @song = Song.find(params[:id])
+    redirect "/songs/#{@song.id}"
+  end
+
 
 
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -4,5 +4,14 @@ class Song < ActiveRecord::Base
   has_many :genres, through: :song_genres
   has_many :song_genres
 
+  def slug
+    self.name.downcase.strip.gsub(" ", "-").gsub(/[^\w-]/, ‘’)
+  end
+
+  def self.find_by_slug(slug)
+    Song.all.find do |song|
+      song.slug == slug
+    end
+  end
 
 end
